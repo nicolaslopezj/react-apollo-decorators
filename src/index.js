ed5f40edd44618033f007097c9e41f54472f4d3b
dsslimshaddy <dsslimshaddy@gmail.com>
2017-08-12 20:01:33 +0530

    add index.js file

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as withGraphQL } from './withGraphQL.js';
+export { default as withFragment } from './withFragment.js';
+export { default as withMutation } from './withMutation.js';
