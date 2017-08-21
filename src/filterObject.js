76021fdcb08fc2636a30314a183b5bb324ea8a08
Nicolás López <nicolaslopezj@me.com>
2017-08-21 17:51:43 -0300

    plain object check

--- src/filterObject.js
+++ src/filterObject.js
@@ -1,7 +1,9 @@
-export default function filterObject (obj, key) {
+import isPlainObject from 'lodash/isPlainObject'
+
+export default function filterObject(obj, key) {
   for (var i in obj) {
     if (!obj.hasOwnProperty(i)) continue
-    if (typeof obj[i] === 'object') {
+    if (isPlainObject(obj[i])) {
       filterObject(obj[i], key)
     } else if (i === key) {
       delete obj[key]
