78b2a7ffe4d6d6c3cac4c176428b7c4347ec4910
Nicolás López <nicolaslopezj@me.com>
2017-08-31 15:01:58 -0300

    fix filter object

--- src/filterObject.js
+++ src/filterObject.js
@@ -1,9 +1,11 @@
 import isPlainObject from 'lodash/isPlainObject'
+import isArray from 'lodash/isArray'
 
 export default function filterObject(obj, key) {
+  console.log('filtering object', obj, key)
   for (var i in obj) {
     if (!obj.hasOwnProperty(i)) continue
-    if (isPlainObject(obj[i])) {
+    if (isPlainObject(obj[i]) || isArray(obj[i])) {
       filterObject(obj[i], key)
     } else if (i === key) {
       delete obj[key]
